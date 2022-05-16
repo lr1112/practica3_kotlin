48c57313f44f985bccb45c00d5cb9150ea171b97
lr1112 <luis_rosario_0324@hotmail.com>
2022-05-16 11:33:17 -0400

    modificando el viewModel

--- app/src/main/java/edu/ucne/registrodepersonas/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrodepersonas/MainActivity.kt
@@ -2,8 +2,18 @@ package edu.ucne.registrodepersonas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.registrodepersonas.databinding.ActivityMainBinding
+import edu.ucne.registrodepersonas.viewmodel.PersonasViewModel
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private val personasViewModel: PersonasViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/edu/ucne/registrodepersonas/viewmodel/PersonasViewModel.kt
@@ -0,0 +1,26 @@
+package edu.ucne.registrodepersonas.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+import edu.ucne.registrodepersonas.data.PersonasDao
+import edu.ucne.registrodepersonas.model.Personas
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PersonasViewModel @Inject constructor(val personasDao: PersonasDao): ViewModel() {
+    private val _guardado = MutableLiveData(false)
+    val guardado: LiveData<Boolean> get() = _guardado
+
+    fun guardar(personas: Personas){
+        viewModelScope.launch {
+            personasDao.insertar(personas)
+            _guardado.value=true
+        }
+    }
+}
+

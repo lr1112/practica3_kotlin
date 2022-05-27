149909d470e3f8f04804c67219a02b16fee36c26
lr1112 <luis_rosario_0324@hotmail.com>
2022-05-27 10:51:22 -0400

    terminando de corregir errores y modificando el layout

--- app/src/main/java/edu/ucne/registrodepersonas/data/PersonasDao.kt
+++ app/src/main/java/edu/ucne/registrodepersonas/data/PersonasDao.kt
@@ -3,7 +3,7 @@ package edu.ucne.registrodepersonas.data
 
 import androidx.room.*
 import edu.ucne.registrodepersonas.model.Personas
-
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -12,8 +12,11 @@ interface PersonasDao {
     @Insert (onConflict = OnConflictStrategy.REPLACE)
     fun insertar (personas: Personas)
 
-   /* @Query("SELECT * FROM Personas ORDER BY nombre DESC")
-    fun getLista(): Flow<List<Personas>>*/
+   @Query("""
+       SELECT * 
+       FROM Personas 
+       """)
+    fun getLista(): Flow<List<Personas>>
 
     @Delete
     suspend fun eliminar (personas: Personas)
